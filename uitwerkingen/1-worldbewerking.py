b3d172f0884213fc15715886c202f4922030b85f
Marcel Haas <mail@marcelhaas.com>
2018-08-31 21:38:08 +0200

    Paar opdrachten en uitwerkingen toegevoegd

---
+++ uitwerkingen/1-worldbewerking.py
@@ -0,0 +1,17 @@
+hw = "Hello World!"  # de definitie van een variabele met de naam hw en als inhoud de string "Hello World!"
+print(hw)
+print(hw.upper())
+print(hw.lower())
+print(len(hw))
+print(hw+' '+hw+' '+hw)
+
+## TIP: Als je hier niet uitkwam: wanneer je de naam van je stringvariabele in een cell typt, 
+##      dan een punt en dan op tab drukt, dan krijg je een lijstje met mogelijkheden!
+
+## TIP: Als je een functie niet herkent of begrijpt, maar wil weten wat hij doet, 
+##      type deze dan in een cell met een vraagteken erachter 
+##      (voor het voorbeeld even het commentaarteken verwijderen):
+# hw.upper?
+
+## TIP: Een dergelijke "docstring" verschijnt ook wanneer je alt-tab-tab indrukt 
+##      tussen de haakjes van de functie.
